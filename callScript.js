f8230a2b2a7ef0dfca765afb61ecce432b08026b
Michael Krausse <michael.krausse@directbox.com>
2018-04-21 23:09:37 +0200

    spezialfall für worte auf oren, wie moderatoren eingefügt spezialfall für eine*r

--- callScript.js
+++ callScript.js
@@ -5,11 +5,18 @@ var x = x.replace(/rInnen/g, "r");
 var x = x.replace(/Innen/g, "en");
 
 // entfernen der Sterne in ihren Varianten
+// - Spezialfälle
+var x = x.replace(/tor\*innen/g, "toren");
+
 var x = x.replace(/r\*innen/g, "r");
 var x = x.replace(/r\*in/g, "r");
+
+// * in der der Normalform
 var x = x.replace(/\*innen/g, "en");
 
 // und noch ein paar verrückte Dinge
 var x = x.replace(/jede\*r/g, "jeder");
+var x = x.replace(/eine\*r/g, "einer");
+var x = x.replace(/sie\*er/g, "er");
 
 document.documentElement.innerHTML = x;
