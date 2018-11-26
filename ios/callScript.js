9d1d5947598643218404bad46992126b335acb9c
Michael Krausse <michael.krausse@directbox.com>
2018-11-26 21:05:28 +0100

    special case for ärzt*Innen

--- ios/callScript.js
+++ ios/callScript.js
@@ -2,11 +2,13 @@ function transform() {
 var x = document.documentElement.innerHTML;
 
 // entfernen des Innen erst die speziellen, dann die allgemeinen
+var x = x.replace(/ztInnen/g, "zte");
 var x = x.replace(/torInnen/g, "toren");
 var x = x.replace(/rInnen/g, "r");
 var x = x.replace(/Innen/g, "en");
 
 // entfernen der Sterne erst die speziellen, dann die allgemeinen
+var x = x.replace(/zt\*innen/g, "zte");
 var x = x.replace(/tor\*innen/g, "toren");
 var x = x.replace(/r\*innen/g, "r");
 var x = x.replace(/r\*in/g, "r");
